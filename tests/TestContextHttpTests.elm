803733e56c510838aa0dca164d188496e90fb30f
Brooke Angel <brooke.nicole.angel@gmail.com>
2019-01-22 17:14:04 -0800

    make TestMsg custom type

--- tests/TestContextHttpTests.elm
+++ tests/TestContextHttpTests.elm
@@ -23,20 +23,24 @@ deconstructEffect testEffect =
             [ TestContext.HttpRequest { method = "GET", url = url } ]
 
 
-type alias TestMsg =
-    TestEffect
+type TestMsg
+    = PassThroughEffect TestEffect
 
 
 start : TestEffect -> TestContext TestMsg () TestEffect
 start initialEffect =
     TestContext.createWithSimulatedEffects
         { init = ( (), initialEffect )
-        , update = \msg () -> ( (), msg )
+        , update =
+            \msg () ->
+                case msg of
+                    PassThroughEffect effect ->
+                        ( (), effect )
         , view =
             \() ->
                 Html.div []
                     [ Html.button
-                        [ onClick (HttpGet "https://example.com/buttons/get") ]
+                        [ onClick (PassThroughEffect (HttpGet "https://example.com/buttons/get")) ]
                         [ Html.text "Get" ]
                     ]
         , deconstructEffect = deconstructEffect
@@ -46,55 +50,57 @@ start initialEffect =
 all : Test
 all =
     describe "TestContext (HTTP API)"
-        [ test "can assert that an HTTP request was made from init (failure)" <|
-            \() ->
-                start NoEffect
-                    |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/" }
-                    |> expectFailure
-                        (String.join "\n"
-                            [ "assertHttpRequest: Expected HTTP request (GET https://example.com/) to have been made, but it was not."
-                            , "    No requests were made."
-                            ]
-                        )
-        , test "can assert that an HTTP request was made from init (success)" <|
-            \() ->
-                start (HttpGet "https://example.com/")
-                    |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/" }
-                    |> expectSuccess
-        , test "can assert that an HTTP request was made from update" <|
-            \() ->
-                start NoEffect
-                    |> TestContext.update (HttpGet "https://example.com/from-update")
-                    |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/from-update" }
-                    |> expectSuccess
-        , test "can assert that an HTTP request was made via a user interaction" <|
-            \() ->
-                start NoEffect
-                    |> TestContext.clickButton "Get"
-                    |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/buttons/get" }
-                    |> expectSuccess
-        , test "error message includes list of pending requests" <|
-            \() ->
-                start (HttpGet "https://example.com/actualRequest")
-                    |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/not-made" }
-                    |> expectFailure
-                        (String.join "\n"
-                            [ "assertHttpRequest: Expected HTTP request (GET https://example.com/not-made) to have been made, but it was not."
-                            , "    The following requests were made:"
-                            , "      - GET https://example.com/actualRequest"
-                            ]
-                        )
-        , test "gives explanatory error when using assertHttpRequest without using createWithSimulatedEffects" <|
-            \() ->
-                TestContext.create
-                    { init = ( (), () )
-                    , update = \() () -> ( (), () )
-                    , view = \() -> Html.text "[view]"
-                    }
-                    |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/" }
-                    |> expectFailure "TEST SETUP ERROR: In order to use assertHttpRequest, you MUST create your TestContext with TestContext.createWithSimulatedEvents"
+        [ describe "assertHttpRequest"
+            [ test "can assert that an HTTP request was made from init (failure)" <|
+                \() ->
+                    start NoEffect
+                        |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/" }
+                        |> expectFailure
+                            (String.join "\n"
+                                [ "assertHttpRequest: Expected HTTP request (GET https://example.com/) to have been made, but it was not."
+                                , "    No requests were made."
+                                ]
+                            )
+            , test "can assert that an HTTP request was made from init (success)" <|
+                \() ->
+                    start (HttpGet "https://example.com/")
+                        |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/" }
+                        |> expectSuccess
+            , test "can assert that an HTTP request was made from update" <|
+                \() ->
+                    start NoEffect
+                        |> TestContext.update (PassThroughEffect (HttpGet "https://example.com/from-update"))
+                        |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/from-update" }
+                        |> expectSuccess
+            , test "can assert that an HTTP request was made via a user interaction" <|
+                \() ->
+                    start NoEffect
+                        |> TestContext.clickButton "Get"
+                        |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/buttons/get" }
+                        |> expectSuccess
+            , test "error message includes list of pending requests" <|
+                \() ->
+                    start (HttpGet "https://example.com/actualRequest")
+                        |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/not-made" }
+                        |> expectFailure
+                            (String.join "\n"
+                                [ "assertHttpRequest: Expected HTTP request (GET https://example.com/not-made) to have been made, but it was not."
+                                , "    The following requests were made:"
+                                , "      - GET https://example.com/actualRequest"
+                                ]
+                            )
+            , test "gives explanatory error when using assertHttpRequest without using createWithSimulatedEffects" <|
+                \() ->
+                    TestContext.create
+                        { init = ( (), () )
+                        , update = \() () -> ( (), () )
+                        , view = \() -> Html.text "[view]"
+                        }
+                        |> TestContext.assertHttpRequest { method = "GET", url = "https://example.com/" }
+                        |> expectFailure "TEST SETUP ERROR: In order to use assertHttpRequest, you MUST create your TestContext with TestContext.createWithSimulatedEvents"
 
-        -- TODO: how to handle multiple requests made to the same method/URL?
+            -- TODO: how to handle multiple requests made to the same method/URL?
+            ]
         ]
 
 
