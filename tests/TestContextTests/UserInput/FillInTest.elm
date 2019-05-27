7eb2331faae37781bdcd5e941c32fc5d5ebedbbf
Aaron VonderHaar <gruen0aermel@gmail.com>
2019-05-27 14:22:15 -0700

    Clean up FillInTest

--- tests/TestContextTests/UserInput/FillInTest.elm
+++ tests/TestContextTests/UserInput/FillInTest.elm
@@ -3,7 +3,7 @@ module TestContextTests.UserInput.FillInTest exposing (all)
 import Expect
 import Html exposing (Html)
 import Html.Attributes exposing (for, id)
-import Html.Events exposing (onClick)
+import Html.Events
 import Test exposing (..)
 import TestContext exposing (TestContext)
 
@@ -13,52 +13,17 @@ type TestEffect
     | LogUpdate String
 
 
-testInit : ( String, TestEffect )
-testInit =
-    ( "<INIT>"
-    , NoOp
-    )
-
-
-testUpdate : String -> String -> ( String, TestEffect )
-testUpdate msg model =
-    ( model ++ ";" ++ msg
-    , LogUpdate msg
-    )
-
-
 handleInput : String -> Html.Attribute String
 handleInput fieldId =
     Html.Events.onInput (\text -> "Input:" ++ fieldId ++ ":" ++ text)
 
 
-testView : String -> Html String
-testView model =
-    Html.div []
-        [ Html.textarea [ handleInput "textarea" ] []
-        , Html.div []
-            [ Html.label [ for "field-1" ] [ Html.text "Field 1" ]
-            , Html.input [ id "field-1", handleInput "field-1" ] []
-            , Html.label [ for "field-2" ] [ Html.text "Field 2" ]
-            , Html.input [ id "field-2", handleInput "field-2" ] []
-            ]
-        , Html.div []
-            [ Html.div [ id "button-a" ]
-                [ Html.button [ onClick "CLICK-A" ] [ Html.text "Ambiguous click" ]
-                ]
-            , Html.div [ id "button-b" ]
-                [ Html.button [ onClick "CLICK-B" ] [ Html.text "Ambiguous click" ]
-                ]
-            ]
-        ]
-
-
-testContext : TestContext String String TestEffect
-testContext =
+start : List (Html String) -> TestContext String String TestEffect
+start view =
     TestContext.create
-        { init = testInit
-        , update = testUpdate
-        , view = testView
+        { init = ( "<INIT>", NoOp )
+        , update = \msg model -> ( model ++ ";" ++ msg, LogUpdate msg )
+        , view = \_ -> Html.node "body" [] view
         }
 
 
@@ -67,28 +32,29 @@ all =
     describe "TestContext.fillIn (and fillInTextArea)"
         [ test "can simulate textarea input" <|
             \() ->
-                testContext
+                start
+                    [ Html.textarea [ handleInput "textarea" ] []
+                    ]
                     |> TestContext.fillInTextarea "ABC"
                     |> TestContext.expectModel (Expect.equal "<INIT>;Input:textarea:ABC")
         , test "can simulate text input on a labeled field" <|
             \() ->
-                testContext
+                start
+                    [ Html.label [ for "field-1" ] [ Html.text "Field 1" ]
+                    , Html.input [ id "field-1", handleInput "field-1" ] []
+                    , Html.label [ for "field-2" ] [ Html.text "Field 2" ]
+                    , Html.input [ id "field-2", handleInput "field-2" ] []
+                    ]
                     |> TestContext.fillIn "field-1" "Field 1" "value99"
                     |> TestContext.expectModel (Expect.equal "<INIT>;Input:field-1:value99")
         , test "can simulate text input on a labeled textarea" <|
             \() ->
-                TestContext.create
-                    { init = testInit
-                    , update = testUpdate
-                    , view =
-                        \_ ->
-                            Html.div []
-                                [ Html.label [ for "field-1" ] [ Html.text "Field 1" ]
-                                , Html.textarea [ id "field-1", handleInput "field-1" ] []
-                                , Html.label [ for "field-2" ] [ Html.text "Field 2" ]
-                                , Html.textarea [ id "field-2", handleInput "field-2" ] []
-                                ]
-                    }
+                start
+                    [ Html.label [ for "field-1" ] [ Html.text "Field 1" ]
+                    , Html.textarea [ id "field-1", handleInput "field-1" ] []
+                    , Html.label [ for "field-2" ] [ Html.text "Field 2" ]
+                    , Html.textarea [ id "field-2", handleInput "field-2" ] []
+                    ]
                     |> TestContext.fillIn "field-1" "Field 1" "value99"
                     |> TestContext.expectModel (Expect.equal "<INIT>;Input:field-1:value99")
         ]
