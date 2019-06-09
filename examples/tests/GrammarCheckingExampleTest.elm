e42efc9f428c7231c1cbb1ca2cf1732e3cf15d26
Aaron VonderHaar <gruen0aermel@gmail.com>
2019-06-09 00:12:12 -0700

    "Testing programs with ports" guide

---
+++ examples/tests/GrammarCheckingExampleTest.elm
@@ -0,0 +1,64 @@
+module GrammarCheckingExampleTest exposing (all)
+
+import Expect
+import GrammarCheckingExample as Main
+import Json.Decode
+import Json.Encode
+import SimulatedEffect.Cmd
+import SimulatedEffect.Ports
+import Test exposing (..)
+import Test.Html.Selector exposing (text)
+import TestContext exposing (TestContext)
+
+
+start : TestContext Main.Msg Main.Model Main.Effect
+start =
+    TestContext.createDocument
+        { init = Main.init
+        , update = Main.update
+        , view = Main.view
+        }
+        |> TestContext.withSimulatedEffects simulateEffects
+        |> TestContext.withSimulatedSubscriptions simulateSub
+        |> TestContext.start ()
+
+
+all : Test
+all =
+    describe "GrammarCheckingExample"
+        [ test "checking grammar" <|
+            \() ->
+                start
+                    |> TestContext.fillIn "main"
+                        "Enter text to check"
+                        "The youngest man the boat."
+                    |> TestContext.clickButton "Check"
+                    |> TestContext.checkAndClearOutgoingPort
+                        "checkGrammar"
+                        Json.Decode.string
+                        (Expect.equal [ "The youngest man the boat." ])
+                    |> TestContext.simulateIncomingPort
+                        "grammarCheckResults"
+                        (Json.Encode.list Json.Encode.string
+                            [ "Garden-path sentences can confuse the reader." ]
+                        )
+                    |> TestContext.expectViewHas
+                        [ text "Garden-path sentences can confuse the reader." ]
+        ]
+
+
+simulateEffects : Main.Effect -> TestContext.SimulatedEffect Main.Msg
+simulateEffects effect =
+    case effect of
+        Main.NoEffect ->
+            SimulatedEffect.Cmd.none
+
+        Main.CheckGrammarEffect text ->
+            SimulatedEffect.Ports.send "checkGrammar" (Json.Encode.string text)
+
+
+simulateSub : Main.Model -> TestContext.SimulatedSub Main.Msg
+simulateSub _ =
+    SimulatedEffect.Ports.subscribe "grammarCheckResults"
+        (Json.Decode.list Json.Decode.string)
+        Main.GrammarCheckResults
