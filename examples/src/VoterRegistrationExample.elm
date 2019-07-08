251e0bdaaf14b07208d1a1c9abaf7d3909f004c3
Aaron VonderHaar <gruen0aermel@gmail.com>
2019-07-07 22:13:35 -0700

    Add VoterRegistrationExample

--- examples/src/VoterRegistrationExample.elm
+++ examples/src/VoterRegistrationExample.elm
@@ -1,15 +1,32 @@
 module VoterRegistrationExample exposing (Model, Msg(..), init, update, view)
 
 import Browser
+import Html
+import Html.Attributes exposing (for, id, type_, value)
+import Html.Events exposing (onClick, onInput, onSubmit)
 import Http
+import Json.Decode
+import Json.Encode
 
 
-type alias Model =
-    {}
+type Model
+    = Form
+        { name : String
+        , streetAddress : String
+        , postalCode : String
+        }
+    | Loading
+    | Success
+        { nextElection : String
+        }
 
 
 type Msg
-    = RegistrationResponse (Result Http.Error String)
+    = ChangeName String
+    | ChangeStreetAddress String
+    | ChangePostalCode String
+    | SubmitRegistration
+    | RegistrationResponse (Result Http.Error String)
 
 
 type alias Flags =
@@ -18,22 +35,118 @@ type alias Flags =
 
 init : Flags -> ( Model, Cmd Msg )
 init () =
-    ( {}
+    ( Form
+        { name = ""
+        , streetAddress = ""
+        , postalCode = ""
+        }
     , Cmd.none
     )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model
-    , Cmd.none
-    )
+    case ( model, msg ) of
+        ( Form info, ChangeName newName ) ->
+            ( Form { info | name = newName }
+            , Cmd.none
+            )
+
+        ( Form info, ChangeStreetAddress newStreetAddress ) ->
+            ( Form { info | streetAddress = newStreetAddress }
+            , Cmd.none
+            )
+
+        ( Form info, ChangePostalCode newPostalCode ) ->
+            ( Form { info | postalCode = newPostalCode }
+            , Cmd.none
+            )
+
+        ( Form info, SubmitRegistration ) ->
+            ( Loading
+            , Http.post
+                { url = "/register"
+                , body =
+                    Http.jsonBody <|
+                        Json.Encode.object
+                            [ ( "name", Json.Encode.string info.name )
+                            , ( "streetAddress", Json.Encode.string info.streetAddress )
+                            , ( "postalCode", Json.Encode.string info.postalCode )
+                            ]
+                , expect =
+                    Http.expectJson
+                        RegistrationResponse
+                        (Json.Decode.field "nextElection" Json.Decode.string)
+                }
+            )
+
+        ( Form _, _ ) ->
+            ( model, Cmd.none )
+
+        ( Loading, RegistrationResponse (Ok nextElection) ) ->
+            ( Success
+                { nextElection = nextElection
+                }
+            , Cmd.none
+            )
+
+        ( Loading, _ ) ->
+            ( model, Cmd.none )
+
+        ( Success _, _ ) ->
+            ( model, Cmd.none )
 
 
 view : Model -> Browser.Document Msg
 view model =
     { title = ""
-    , body = []
+    , body =
+        [ case model of
+            Form info ->
+                -- TODO: should use onSubmit instead of an onClick button
+                Html.form []
+                    [ Html.label
+                        [ for "name" ]
+                        [ Html.text "Name" ]
+                    , Html.input
+                        [ id "name"
+                        , onInput ChangeName
+                        , value info.name
+                        ]
+                        []
+                    , Html.label
+                        [ for "street-address" ]
+                        [ Html.text "Street Address" ]
+                    , Html.input
+                        [ id "street-address"
+                        , onInput ChangeStreetAddress
+                        , value info.streetAddress
+                        ]
+                        []
+                    , Html.label
+                        [ for "postcode" ]
+                        [ Html.text "Postal Code" ]
+                    , Html.input
+                        [ id "postcode"
+                        , onInput ChangePostalCode
+                        , value info.postalCode
+                        ]
+                        []
+                    , Html.button
+                        [ onClick SubmitRegistration ]
+                        [ Html.text "Register" ]
+                    ]
+
+            Loading ->
+                Html.text "Loading..."
+
+            Success info ->
+                Html.div []
+                    [ Html.text "Success!"
+                    , Html.hr [] []
+                    , Html.text ("Next election date is: " ++ info.nextElection)
+                    ]
+        ]
     }
 
 
